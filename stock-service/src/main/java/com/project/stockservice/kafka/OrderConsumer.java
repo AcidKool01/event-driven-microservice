e5b2e4e67ce8384f18a785b6315fc7b7ffc88b20
Tushar <tushar01kumar28@gmail.com>
2023-03-13 00:11:17 +0530

    added database

--- stock-service/src/main/java/com/project/stockservice/kafka/OrderConsumer.java
+++ stock-service/src/main/java/com/project/stockservice/kafka/OrderConsumer.java
@@ -1,6 +1,8 @@
 package com.project.stockservice.kafka;
 
 import com.project.basedomains.dto.OrderEvent;
+import com.project.stockservice.entity.StockData;
+import com.project.stockservice.repository.StockDataRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -9,6 +11,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class OrderConsumer {
 
+    private StockDataRepository dataRepository;
+
+    public OrderConsumer(StockDataRepository dataRepository) {
+        this.dataRepository = dataRepository;
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
 
     @KafkaListener( topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
@@ -16,5 +24,11 @@ public class OrderConsumer {
         LOGGER.info(String.format("Order Event received in Stock Service => %s", event.toString()));
 
         // save the order event in the database
+
+        StockData stockData = new StockData();
+        stockData.setStockEventData(event.toString());
+
+        dataRepository.save(stockData);
+
     }
 }
